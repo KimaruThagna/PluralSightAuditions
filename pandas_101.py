b34a17adf1d183381f91f523d1b26b1798c41c44
Kimaru thagana <macbuntu@localhost.localdomain>
2020-02-13 11:32:41 +0300

    create dataframe

--- pandas_101.py
+++ pandas_101.py
@@ -1,1 +1,5 @@
 import pandas as pd
+import numpy as np
+ #create sample dataframe
+df = pd.DataFrame([[np.nan, 2, np.nan, 0], [3, 4, np.nan, 1], [np.nan, np.nan, np.nan, 5],[3, 4, np.nan, 1], [3, 4, 0, 1]], columns=list('ABCD'))
+print(df.shape())
